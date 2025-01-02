d7780801e1b896dedb2c26636cae763d9a0e3cc3
komace <30932472+komace@users.noreply.github.com>
2025-01-03 00:48:11 +0100

    Final Test

---
+++ Test_02.py
@@ -0,0 +1,33 @@
+import turtle
+
+def draw_tree(branch_length, level, angle):
+    if level == 0:
+        return
+    
+    turtle.forward(branch_length)
+    turtle.left(angle)
+    draw_tree(branch_length * 0.7, level - 1, angle)
+    turtle.right(2 * angle)
+    draw_tree(branch_length * 0.7, level - 1, angle)
+    turtle.left(angle)
+    turtle.backward(branch_length)
+
+def main():
+    # Ініціалізація екрану та черепахи
+    turtle.speed("fastest")
+    turtle.left(90)
+    turtle.up()
+    turtle.backward(100)
+    turtle.down()
+    
+    # Отримання рівня рекурсії від користувача
+    level = int(input("Введіть рівень рекурсії: "))
+    
+    # Малювання дерева
+    draw_tree(100, level, 30)
+    
+    # Завершення малювання
+    turtle.done()
+
+if __name__ == "__main__":
+    main()
---
+++ Test_03.py
@@ -0,0 +1,55 @@
+import heapq
+
+class Graph:
+    def __init__(self):
+        self.edges = {}
+        self.nodes = set()
+
+    def add_node(self, value):
+        self.nodes.add(value)   
+        self.edges[value] = []
+
+    def add_edge(self, from_node, to_node, weight):
+        self.edges[from_node].append((to_node, weight))
+        #self.edges[to_node].append((from_node, weight))    
+
+graph = Graph()
+graph.add_node('A')
+graph.add_node('B')
+graph.add_node('C')
+graph.add_node('D')
+graph.add_node('E')
+
+graph.add_edge('A', 'B', 1)
+graph.add_edge('A', 'C', 4)
+graph.add_edge('B', 'C', 2)
+graph.add_edge('B', 'D', 5)
+graph.add_edge('C', 'D', 1)
+graph.add_edge('D', 'E', 3)
+
+def dijkstra(graph, start):
+    distances = {node: float('infinity') for node in graph.nodes}
+    distances[start] = 0
+    priority_queue = [(0, start)]
+    
+    
+    while priority_queue:
+        current_distance, current_node = heapq.heappop(priority_queue)
+        
+        if current_distance > distances[current_node]:
+            continue
+        
+        for neighbor, weight in graph.edges[current_node]:
+            distance = current_distance + weight
+            
+            if distance < distances[neighbor]:
+                distances[neighbor] = distance
+                heapq.heappush(priority_queue, (distance, neighbor))
+    
+    return distances
+
+
+start_node = 'A'
+distances = dijkstra(graph, start_node)
+for node in distances:
+    print(f'Шлях з {start_node} до {node} = {distances[node]}')
